b5c34d76d68af03792a548265c179cdc222f2226
ben_astengo <astengo93@gmail.com>
2022-01-12 11:44:35 -0600

    Adds black market body parts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Product.create(name: "Heart", price: 130, description: "The source of love")
+Product.create(name: "Brain", price: 130000, description: "an organ of soft nervous tissue contained in the skull of vertebrates, functioning as the coordinating center of sensation and intellectual and nervous activity." )
+Product.create(name: "Foot", price: 20, description: "the lower extremity of the leg below the ankle, on which a person stands or walks.")
+Product.create(name: "Gallbladder", price: 0, description: "the small sac-shaped organ beneath the liver, in which bile is stored after secretion by the liver and before release into the intestine.")
