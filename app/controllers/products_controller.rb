1046bc2c25a9467d78c90e360ee4c22f9c6da4ad
ben_astengo <astengo93@gmail.com>
2022-01-12 12:00:28 -0600

    Adds routes for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,25 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def heart_method
+    products = Product.find_by(name: "Heart")
+    render json: products.as_json
+  end
+
+  def brain_method
+    products = Product.find_by(name: "Brain" )
+    render json: products.as_json
+  end
+
+  def foot_method
+    products = Product.find_by(name: "Foot" )
+    render json: products.as_json
+  end
+
+  def gallbladder_method
+    products = Product.find_by(name: "Gallbladder")
+    render json: products.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "/black_market" => "products#black_market_method"
+  get "/heart" => "products#heart_method"
+  get "/brain" => "products#brain_method"
+  get "/foot" => "products#foot_method"
+  get "/gallbladder" => "products#gallbladder_method"
 
 end
